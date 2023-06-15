328113ead704a22b5b4168a15ce192fa55a3c98c
aminoxlhd <aminoxlhd@gmail.com>
2023-06-15 11:24:35 -0700

    add 4 and 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'main printf'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
