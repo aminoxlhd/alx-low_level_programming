dc2f83a21ffdd0e85172f4522869f0b20805c344
aminoxlhd <aminoxlhd@gmail.com>
2023-06-15 11:54:04 -0700

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: 'main quote'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
+}
+
