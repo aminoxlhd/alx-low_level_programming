24697815cf9ff8f94982455effb51d6cdbf56138
aminoxlhd <aminoxlhd@gmail.com>
2023-06-16 17:28:48 -0700

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ab;
+
+	for (ab = 0; ab < 10; ab++)
+		printf("%d", ab);
+	printf("\n");
+	return (0);
+}
