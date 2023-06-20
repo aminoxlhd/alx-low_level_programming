d9f60e71a8b3daa5e36b93afb4297d2178845fde
aminoxlhd <aminoxlhd@gmail.com>
2023-06-19 22:02:57 -0700

    add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/*
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
