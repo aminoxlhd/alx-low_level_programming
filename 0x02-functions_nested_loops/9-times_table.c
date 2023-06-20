5a7d443d8d3695493e5589a5bd75862a12567c6d
aminoxlhd <aminoxlhd@gmail.com>
2023-06-20 13:12:31 -0700

    edit 2 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,25 +15,18 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 		c = a * b;
-		if (c == 0)
-		{
-			_putchar(c + '0');
-		}
-		if (c < 10 && c != 0)
+
+		if (b > 0)
 		{
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
-			_putchar(c + '0');
 		}
-		else if (c >= 10)
+		if (c < 10)
 		{
-			_putchar(',');
 			_putchar(' ');
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
 		}
-	}
+		_putchar(c + '0');
+		}
 	_putchar('\n');
 	}
 }
