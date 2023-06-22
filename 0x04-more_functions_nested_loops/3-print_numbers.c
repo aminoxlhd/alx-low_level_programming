78ce60ece4d35dc20c352ab696978702385933ff
aminoxlhd <aminoxlhd@gmail.com>
2023-06-22 14:26:02 -0700

    add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int ab;
+
+	for (ab = 48; ab < 57; ab++)
+	{
+		_putchar(ab);
+	}
+	_putchar("\n");
+}
