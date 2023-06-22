b9b412bcdf517aa71e7dfe470270724296f4b7f2
aminoxlhd <aminoxlhd@gmail.com>
2023-06-21 19:46:56 -0700

    edit 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,13 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
-	int a;
+	int i = 0;
+	/* Iniitialize i with 0 */
 
-	a = 0;
-	positive_or_negative(a);
+	positive_or_negative(i);
 
 	return (0);
 }
