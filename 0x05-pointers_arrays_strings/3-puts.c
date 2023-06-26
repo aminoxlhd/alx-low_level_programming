654b00b0209b207dd17fc95f6f27c648b52ce5a7
aminoxlhd <aminoxlhd@gmail.com>
2023-06-26 11:06:48 -0700

    add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _puts - prints a string
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
