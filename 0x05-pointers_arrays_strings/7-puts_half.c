43f80145342416d4fbaa7ca52bf29f9bb4c304ba
aminoxlhd <aminoxlhd@gmail.com>
2023-06-26 18:47:50 -0700

    add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * puts_half - half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len;
+	int n;
+	int a;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (a = len / 2; str[a] != '\0'; a++)
+		{
+			_putchar(str[a]);
+		}
+	}
+		else if (len % 2)
+		{
+			for (a = (len - 1) / 2; n < len - 1; a++)
+			{
+				_putchar(str[n + 1]);
+			}
+		}
+		_putchar('\n');
+}
