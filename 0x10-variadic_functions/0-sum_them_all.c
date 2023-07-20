05f196e918bb02f46ae585b91dc4717befa991ce
aminoxlhd <aminoxlhd@gmail.com>
2023-07-20 12:45:22 -0700

    add 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - returns the sum of all its parameters
- * @n: amount of argument
- * Return: If n == 0, return 0
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
