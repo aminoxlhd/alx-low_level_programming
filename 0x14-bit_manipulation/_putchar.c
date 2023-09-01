1fad46bbb71c7935ec6edbe5fe78fefe5959fa1d
root <aminoxlhd>
2023-08-31 18:51:18 -0700

    add 1-print_binary.c

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * _putchar : uses printf to write a character
+ */
+
+
+void _putchar(char c){
+    printf("%d", c);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+void _putchar(char c);
 
 
 #endif
