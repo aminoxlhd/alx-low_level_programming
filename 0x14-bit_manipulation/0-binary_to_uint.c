2a8bdf161b2208f1b6f6157edae671e75c4a889d
root <aminoxlhd>
2023-09-01 18:12:12 -0700

    edit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		value = 2* value + (b[i] - '0');
+		Value = 2* Value + (b[i] - '0');
 	}
 	return (Value);
 }
