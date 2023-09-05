28d19429a3b5e720cececb4d7bef5c7ad052bb96
root <aminoxlhd>
2023-09-04 19:52:09 -0700

    edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	return (0);
 }
 
-	buff = malloc(sizeof(char) *letters);
+	buff = malloc(sizeof(char) * letters);
 
 	if (buff == NULL)
 {
