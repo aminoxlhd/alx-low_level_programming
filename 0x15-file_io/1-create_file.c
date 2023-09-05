21f5bf13198c33b8ec15317b4c3343dd40d194ff
root <aminoxlhd>
2023-09-04 20:07:32 -0700

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-	for (lenght = 0; text_content[lenght];)
+	for (lenght = 0; text_content[lenght])
 		{
 			lenght++;
 		}
