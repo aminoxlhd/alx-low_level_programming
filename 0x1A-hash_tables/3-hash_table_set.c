061e4b599e0511b3cc8341e968ced17a0a5900c2
root <aminoxlhd>
2023-11-03 13:42:11 -0700

    edit 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,6 +35,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node = malloc(sizeof(hash_node_t *));
 
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
 	if (new_node == NULL)
 		return (0);
 	new_node->next = ht->array[index];
